798f95f3b451772f7d6891ecfb7016f596543177
Bogarakesh123 <bogarakesh770@gmail.com>
2024-09-02 14:29:02 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,1 @@
-@echo off
-REM Set up environment (adjust paths as needed)
-set CONDA_HOME=C:\ProgramData\anaconda3
-set PATH=%CONDA_HOME%\Scripts;%PATH%
-
-REM Check Python version
-python --version
-
-REM Run pytest
 pytest -v -s testcases\test_03.py
